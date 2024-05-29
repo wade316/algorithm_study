fa4c736e7a7004120f0f638259f9a1804be2dbe2
wade316 <sdc1565@gmail.com>
2024-05-29 19:08:40 +0900

    유효한 회문 Palindrome 문제풀이

--- app/src/main/java/com/example/algorithm_study/Algorithm/Palindrome.kt
+++ app/src/main/java/com/example/algorithm_study/Algorithm/Palindrome.kt
@@ -1,3 +1,16 @@
 package com.example.algorithm_study.Algorithm
 
+fun main() {
+    val solution = Solution()
 
+    println(solution.isPalindrome("A man, a plan, a canal: Panama"))
+    println(solution.isPalindrome("race a car"))
+    println(solution.isPalindrome(" "))
+}
+
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        val result = s.filter{ it.isLetterOrDigit()}.toLowerCase()
+        if (result == result.reversed()) return true else return false
+    }
+}
